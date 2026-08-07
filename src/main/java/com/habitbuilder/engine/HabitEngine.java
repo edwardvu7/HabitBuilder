5cee780a5c3551b92da99a38c8f62a9ab65a7a1e
Edward <edwardvu7@gmail.com>
2026-08-06 23:07:53 -0700

    need to fix pom.xml error

---
+++ src/main/java/com/habitbuilder/engine/HabitEngine.java
@@ -0,0 +1,5 @@
+package main.java.com.habitbuilder.engine;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class HabitEngine
---
+++ src/main/java/com/habitbuilder/engine/HabitEngineApplication.java
@@ -0,0 +1,19 @@
+package com.habitbuilder.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HabitEngineApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(HabitEngineApplication.class, args);
+
+
+
+    }
+
+
+
+}
